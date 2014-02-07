42f2b757b273d9b661f7c6180b246e7e0168e950
ekta-verma <Ekta_Verma@external.mckinsey.com>
2014-02-07 12:39:35 -0800

    setting ZipCode values using SOAP call

--- app/models/zip_code.rb
+++ app/models/zip_code.rb
@@ -1,5 +1,16 @@
 class ZipCode
   attr_reader :state, :city, :area_code, :time_zone
   	def initialize(zip)
-  end 
+  		client = Savon.client(wsdl: "http://www.webservicex.net/uszip.asmx?WSDL")
+  		response = client.call(:get_info_by_zip, message: { us_zip: zip })
+  		if response.success?
+     	 	data = response.to_array(:get_info_by_zip_response, :get_info_by_zip_result, :new_data_set, :table).first
+      		if data
+			    @state = data[:state]
+			    @city = data[:city]
+			    @area_code = data[:area_code]
+			    @time_zone = data[:time_zone]
+      		end
+    	end
+  	end 
 end
